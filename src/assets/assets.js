9a868442bafd81dacf46ed04479d438974b73c1c
Nathan Guimarães <33322455+nathanguimaraes@users.noreply.github.com>
2024-08-08 19:57:07 -0300

    updates design

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -84,35 +84,35 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Floral",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Cítrico",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Amadeirado",
         menu_image: menu_3
     },
     {
-        menu_name: "Sandwich",
+        menu_name: "Oriental",
         menu_image: menu_4
     },
     {
-        menu_name: "Cake",
+        menu_name: "Frutado",
         menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
+        menu_name: "Aquático",
         menu_image: menu_6
     },
     {
-        menu_name: "Pasta",
+        menu_name: "Gourmand",
         menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
+        menu_name: "Fougère",
         menu_image: menu_8
     }]
 
